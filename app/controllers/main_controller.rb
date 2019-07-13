5cd00a7059fd84304e26b528472b1a7d1864ce31
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-14 01:20:19 +0900

    とりあえずプロジェクト画面からメイン画面に飛べるようにした。 ※メイン画面はまだ何も触っていない。

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  unloadable
+  before_filter :find_project
+  
+  def index
+  end
+
+private
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
+end
